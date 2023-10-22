8f8c76c22a9c592f87b80e434e5f3cfc30555145
kiiton8874 <r.kitagawa8874@gmail.com>
2023-10-22 19:22:03 +0900

    kouzouka

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,68 @@ import { View, Text, StyleSheet } from "react-native";
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>在庫管理</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>画像</Text>
+          </View>
+          <View>
+            <Text>商品名</Text>
+            <Text>2023年10月23日</Text>
+          </View>
+          <View>
+            <Text>800個</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>画像</Text>
+          </View>
+          <View>
+            <Text>商品名</Text>
+            <Text>2023年10月23日</Text>
+          </View>
+          <View>
+            <Text>800個</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>画像</Text>
+          </View>
+          <View>
+            <Text>商品名</Text>
+            <Text>2023年10月23日</Text>
+          </View>
+          <View>
+            <Text>800個</Text>
+          </View>
+        </View>
+
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
